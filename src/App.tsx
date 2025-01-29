c5aaa56f6933c1d018fc8ee31431bd21247286ce
William Lempinen <wlempin@gmail.com>
2025-01-29 16:20:36 +0200

    add react bits

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import TextPressure from './blocks/TextAnimations/TextPressure/TextPressure'
+
 const App = () => {
-  return <div>Hello world</div>
+  return (
+    <div style={{ position: 'relative', height: '300px' }}>
+      <TextPressure
+        text="Hello!"
+        flex={true}
+        alpha={false}
+        stroke={false}
+        width={true}
+        weight={true}
+        italic={true}
+        textColor="#ffffff"
+        strokeColor="#ff0000"
+        minFontSize={36}
+      />
+    </div>
+  )
 }
 
 export default App
